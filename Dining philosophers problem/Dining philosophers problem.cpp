99d40a9568fd7490e6bf17b010f04b9f6888514c
AdamOrłowskii <aorlowski644@gmail.com>
2025-03-21 19:16:09 +0100

    code push

---
+++ Dining philosophers problem/Dining philosophers problem.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+const int number_of_philosophers = 5; // 5 philosophers
+mutex forks[number_of_philosophers]; // 5 forks
+
+void philosopher(int id) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 1000);
+
+    while (true) {
+
+        // thinking
+        cout << "Philosopher " << id << " is thinking...\n";
+        this_thread::sleep_for(chrono::milliseconds(dist(gen)));
+
+        // picking up forks
+        int left = id;
+        int right = (id + 1) % number_of_philosophers;
+
+
+
+
+    }
+
+}
+
+int main()
+{
+
+}
