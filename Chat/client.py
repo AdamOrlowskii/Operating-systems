d93ccceb6fb9c9c4ec35158176d5ce2215be875e
Adam Orłowski <aorlowski644@gmail.com>
2025-06-05 21:56:54 +0200

    Messages are working

--- Chat/client.py
+++ Chat/client.py
@@ -4,6 +4,43 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 
+def listen_for_messages_from_server(client):
+
+    while True:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            username = message.split("~")[0]
+            content = message.split('~')[1]
+            print(f"[{username}] {content}")
+        else:
+            print("Received message is empty")
+
+
+def send_message_to_server(client):
+
+    while True:
+        message = input("Message: ")
+        if message != '':
+            client.sendall(message.encode())
+        else:
+            print("Empty message")
+            exit(0)
+
+
+def communicate_to_server(client):
+
+    username = input("Enter username: ")
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        print("Username cannot be empty")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+
+    send_message_to_server(client)
+
+
 def main():
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +49,8 @@ def main():
         print("Successfully connected to the server")
     except:
         print(f"Unable to connect to server {HOST} and port {PORT}")
+    
+    communicate_to_server(client)
 
 if __name__ == '__main__':
     main()
